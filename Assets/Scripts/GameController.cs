2b49b6e44d725ad3eafd78fe241187cc7f78c50e
Okan Uslu <gd.okanuslu@gmail.com>
2020-02-01 00:56:33 +0300

    Add new people

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public float lvlTimeSecond = 20f; //Levelin süresi
+    
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lvlTimeSecond -= Time.deltaTime;
+        timeText.text = "00:" + (int)lvlTimeSecond;
+
+        if (lvlTimeSecond < 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
