906568f72384db1c3fa442ae7722f2bc64ea8486
berat.mehmetoglu <berat.mehmetoglu@kentkart.com.tr>
2020-02-01 00:36:12 +0300

    gf updated

---
+++ Assets/Vision.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour
+{
+    public GameObject Heart, BreakHeart,GirlFriend;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VisionLook();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Heart.SetActive(false);
+            BreakHeart.SetActive(true);
+        }
+       
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Heart.SetActive(true);
+            BreakHeart.SetActive(false);
+        }
+    }
+
+    private void VisionLook()
+    {
+        
+    }
+}
